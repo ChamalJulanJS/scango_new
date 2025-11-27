0eabfe104a7f98d930f2e654521cd95173ce1964
ChamalJulanJS <0618julajs@gmail.com>
2025-11-27 09:08:26 +0530

    Stop tracking config file to secure API key

--- lib/utils/config.dart
+++
@@ -1,5 +0,0 @@
-class AppConfig {
-  // Your Gemini API key - Get one at https://aistudio.google.com/
-  static const String geminiApiKey = 'AIzaSyDAql7Fy_J1xdpYd4y3dW8beCciY4_hhQo';
-  static bool fromDropDown = false;
-}
